b0c66b3f25eda6bc5f5b112f12ba3b50b9ac4173
mrabalogu <chineduabalogu@yahoo.com>
2017-04-03 14:36:41 +0100

    deploy time

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -6,7 +6,6 @@ defmodule Web.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
